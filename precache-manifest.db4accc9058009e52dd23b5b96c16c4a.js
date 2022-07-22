0e2b2b3037a27cf098c991de0480f40ed59a8dff
Auroob2301 <amjadauroob18@gmail.com>
2022-07-23 03:19:47 +0500

    Updates

---
+++ precache-manifest.db4accc9058009e52dd23b5b96c16c4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce92b92a0dd96d5305a4",
+    "url": "/My_app1-React/static/css/main.3804a5e6.chunk.css"
+  },
+  {
+    "revision": "ce92b92a0dd96d5305a4",
+    "url": "/My_app1-React/static/js/main.ce92b92a.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/My_app1-React/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/My_app1-React/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "c343b59cefec843c0062",
+    "url": "/My_app1-React/static/js/runtime~main.c343b59c.js"
+  },
+  {
+    "revision": "62ded119fdf743e1886c3c47aa5449f9",
+    "url": "/My_app1-React/index.html"
+  }
+];
